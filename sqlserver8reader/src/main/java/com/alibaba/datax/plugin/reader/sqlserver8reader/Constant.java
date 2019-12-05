e1ee93940ea10b01cb92f5ab1ad234836f400772
stringpeng <stringpeng@hotmail.com>
2019-12-05 14:52:24 +0800

    添加sqlserver 8 版本读取插件

---
+++ sqlserver8reader/src/main/java/com/alibaba/datax/plugin/reader/sqlserver8reader/Constant.java
@@ -0,0 +1,7 @@
+package com.alibaba.datax.plugin.reader.sqlserver8reader;
+
+public class Constant {
+
+    public static final int DEFAULT_FETCH_SIZE = 1024;
+
+}
---
+++ sqlserver8reader/src/main/java/com/alibaba/datax/plugin/reader/sqlserver8reader/Key.java
@@ -0,0 +1,6 @@
+package com.alibaba.datax.plugin.reader.sqlserver8reader;
+
+public class Key {
+
+    public static final String FETCH_SIZE = "fetchSize";
+}
